07bba767ee332cfc6aaafd8af936d9d24b429f7d
Daniel Lopez <d@niel.to>
2025-03-08 16:53:57 -0800

    refactor(footer): use tailwind class to set span position instead of custom style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -23,58 +23,58 @@ export default function Footer() {
             alert('menu')
           }}
         >
-          <span className='-translate-1/2 absolute left-1/2 top-1/2 size-12 [@media(pointer:fine)]:hidden'></span>
+          <span className='absolute left-1/2 top-1/2 size-12 translate-x-[-50%] translate-y-[-50%] [@media(pointer:fine)]:hidden'></span>
           <Bars2Icon className='h-6 w-6' />
         </button>
       </div>
       <div className='flex space-x-6'>
         <button
-          className='text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:text-cb-light-blue'
+          className='relative text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:text-cb-light-blue'
           type='button'
           onClick={() => {
             alert('share')
           }}
         >
-          <span className='-translate-1/2 absolute left-1/2 top-1/2 size-12 [@media(pointer:fine)]:hidden'></span>
+          <span className='absolute left-1/2 top-1/2 size-12 translate-x-[-50%] translate-y-[-50%] [@media(pointer:fine)]:hidden'></span>
           <ShareIcon className='h-6 w-6' />
         </button>
         <button
-          className='text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:text-cb-light-blue'
+          className='relative text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:text-cb-light-blue'
           type='button'
           onClick={() => {
             alert('list')
           }}
         >
-          <span className='-translate-1/2 absolute left-1/2 top-1/2 size-12 [@media(pointer:fine)]:hidden'></span>
+          <span className='absolute left-1/2 top-1/2 size-12 translate-x-[-50%] translate-y-[-50%] [@media(pointer:fine)]:hidden'></span>
           <ListBulletIcon className='h-6 w-6' />
         </button>
         <button
-          className='text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:text-cb-light-blue'
+          className='relative text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:text-cb-light-blue'
           type='button'
           onClick={() => {
             alert('settings')
           }}
         >
-          <span className='-translate-1/2 absolute left-1/2 top-1/2 size-12 [@media(pointer:fine)]:hidden'></span>
+          <span className='absolute left-1/2 top-1/2 size-12 translate-x-[-50%] translate-y-[-50%] [@media(pointer:fine)]:hidden'></span>
           <Cog6ToothIcon className='h-6 w-6' />
         </button>
         <button
-          className='text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:text-cb-light-blue'
+          className='relative text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:text-cb-light-blue'
           type='button'
           onClick={() => {
             alert('tools')
           }}
         >
-          <span className='-translate-1/2 absolute left-1/2 top-1/2 size-12 [@media(pointer:fine)]:hidden'></span>
+          <span className='absolute left-1/2 top-1/2 size-12 translate-x-[-50%] translate-y-[-50%] [@media(pointer:fine)]:hidden'></span>
           <WrenchIcon className='h-6 w-6' />
         </button>
         <button
-          className='text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:opacity-25'
+          className='relative text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:opacity-25'
           onClick={() => {
             alert('default')
           }}
         >
-          <span className='-translate-1/2 absolute left-1/2 top-1/2 size-12 [@media(pointer:fine)]:hidden'></span>
+          <span className='absolute left-1/2 top-1/2 size-12 translate-x-[-50%] translate-y-[-50%] [@media(pointer:fine)]:hidden'></span>
           <PencilSquareIcon className='h-6 w-6' />
         </button>
       </div>
